3abd6e07edcb6272412b37c127f633f1b19d4e1d
SeungMinLee <seunlee2@student.42seoul.kr>
2023-12-19 18:32:17 +0900

    Do not use using

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -5,23 +5,20 @@ namespace out
 	char outChar;
 }
 
-using namespace std;
-using namespace out;
-
 int main(int argc, char **argv)
 {
 	if (argc == 1)
-		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << endl;
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	else
 	{
 		for (int i = 1; i < argc; i++)
 		{
 			for (int j = 0; argv[i][j]; j++)
 			{
-				outChar = argv[i][j];
-				cout << outChar;
+				out::outChar = argv[i][j];
+				std::cout << out::outChar;
 			}
 		}
-		cout << endl;
+		std::cout << std::endl;
 	}
 }
